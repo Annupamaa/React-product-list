85ab1f6b8b5612cb0bd36f9a84bbb17812c04eb9
Annupamaa <annu242005@gmail.com>
2024-03-14 00:14:53 +0530

    Product-list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React ,{ useEffect, useState } from "react";
+import axios from "axios";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      let response = await axios.get("https://dummyjson.com/products");
+      setData(response.data.products);
+    }
+    
+    fetchData();
+    
+  },[]);
+
+  // console.log(data);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="box">
+      {data.map((value, index) => 
+        <ul key={index} className="details">
+          { <img src={value.images[0]}/> }
+          <h3>{value.title}</h3>
+          <li>{value.description}</li>
+          <button>Buy : ${value.price}</button>
+        </ul>
+      )}
     </div>
   );
-}
+};
 
 export default App;
